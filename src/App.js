8e1ece40210b6593d4ca4648561019e8984e8641
kaysben <kaysbenbarka@gmail.com>
2021-11-08 00:49:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import kak from './arrd.png';
+
+function App() {
+  return (
+    <div className="App">
+     <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 class="title red">react checkpoint</h1>
+
+<br/>
+
+<img src={kak} alt = 'me'/>
+
+<br/>
+
+<img src="/arr.png" alt ="me"/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4"  type="video/mp4" alt = "video" />
+
+</video>
+    </div>
+    
+  );
+}
+
+export default App;
